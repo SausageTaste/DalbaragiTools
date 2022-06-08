f17f6b9be096163a5128796577ad803b49978b13
Sungmin Woo <woos8899@gmail.com>
2022-06-08 22:10:16 +0900

    Update test_2.cpp

--- test/test_2.cpp
+++ test/test_2.cpp
@@ -7,11 +7,21 @@
 const char* const TEST_MSG = "This is cool";
 
 
-bool test_key_export_import(const dal::crypto::IKey& key) {
-    const auto hex = key.make_hex_str();
-    const auto bin = dal::crypto::IKey{hex};
+namespace {
 
-    return key == bin;
+    bool test_key_export_import(const dal::crypto::IKey& key) {
+        dal::crypto::KeyAttrib attrib;
+        attrib.m_owner_name = "Sungmin Woo";
+        attrib.m_email = "woos8899@gmail.com";
+        attrib.m_description = "Test";
+
+        const auto build1 = dal::crypto::build_key_store(key, attrib);
+        const auto parse1 = dal::crypto::parse_key_store(build1, "");
+        const auto build2 = dal::crypto::build_key_store(parse1.first, parse1.second);
+
+        return build1 == build2;
+    }
+
 }
 
 
@@ -22,9 +32,9 @@ int main() {
     const auto [public_key, secret_key] = sign_mgr.gen_keys();
     const auto [public_key2, secret_key2] = sign_mgr.gen_keys();
 
-    if (!test_key_export_import(public_key))
+    if (!::test_key_export_import(public_key))
         return 1;
-    if (!test_key_export_import(secret_key))
+    if (!::test_key_export_import(secret_key))
         return 2;
 
     const auto signature = sign_mgr.create_signature(TEST_MSG, std::strlen(TEST_MSG), secret_key);
