a1ed4c7bbd3f5eb04198106eebd0f93d1d410424
Sungmin Woo <woos8899@gmail.com>
2022-06-17 18:36:20 +0900

    Signing while compiling

--- app/main.cpp
+++ app/main.cpp
@@ -463,6 +463,10 @@ namespace {
 
         parser.add_argument("operation")
             .help("Operation name");
+
+        parser.add_argument("-k", "--key")
+            .help("Path to a secret key file");
+
         parser.add_argument("files")
             .help("Input model file paths")
             .remaining();
@@ -483,13 +487,22 @@ namespace {
             }
 
             const auto model = dal::parser::convert_to_model_dmd(scenes.at(0));
-            const auto binary_built = dal::parser::build_binary_model(model, nullptr, nullptr);
+            dal::parser::binary_buffer_t binary_built;
+            const auto key_path = parser.present("--key");
+            if (key_path.has_value()) {
+                dal::crypto::PublicKeySignature sign_mgr{ dal::crypto::CONTEXT_PARSER };
+                const auto [key, attrib] = dal::crypto::load_key<dal::crypto::PublicKeySignature::SecretKey>(key_path->c_str());
+                binary_built = dal::parser::build_binary_model(model, &key, &sign_mgr).value();
+            }
+            else {
+                binary_built = dal::parser::build_binary_model(model, nullptr, nullptr).value();
+            }
 
             std::filesystem::path output_path = src_path;
             output_path.replace_extension("dmd");
 
             std::ofstream file(output_path.u8string().c_str(), std::ios::binary);
-            file.write(reinterpret_cast<const char*>(binary_built->data()), binary_built->size());
+            file.write(reinterpret_cast<const char*>(binary_built.data()), binary_built.size());
             file.close();
         }
     }
