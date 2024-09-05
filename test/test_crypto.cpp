b1d9d912002d13faa31934b5ec8ce8d0c3d34497
Sungmin Woo <woos8899@gmail.com>
2024-09-05 15:34:38 +0900

    New crypto test code

---
+++ test/test_crypto.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "daltools/common/crypto.h"
+
+
+namespace {
+
+    TEST(Crypto, KeyGen) {
+
+    }
+
+}
+
+
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
