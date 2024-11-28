a8eba844ad92dfae2dc65dc0142a15cb78418e1e
Sungmin Woo <woos8899@gmail.com>
2024-11-28 11:03:26 +0900

    `vec_cast` for `vec3`

--- include/daltools/common/glm_tool.hpp
+++ include/daltools/common/glm_tool.hpp
@@ -28,4 +28,26 @@ namespace dal {
         return *reinterpret_cast<const sung::TVec2<T>*>(&v);
     }
 
+    template <typename T>
+    glm::tvec3<T>& vec_cast(sung::TVec3<T>& v) {
+        static_assert(sizeof(glm::tvec3<T>) == sizeof(T) * 3);
+        static_assert(sizeof(glm::tvec3<T>) == sizeof(sung::TVec3<T>));
+        return *reinterpret_cast<glm::tvec3<T>*>(&v);
+    }
+
+    template <typename T>
+    const glm::tvec3<T>& vec_cast(const sung::TVec3<T>& v) {
+        return *reinterpret_cast<const glm::tvec3<T>*>(&v);
+    }
+
+    template <typename T>
+    sung::TVec3<T>& vec_cast(glm::tvec3<T>& v) {
+        return *reinterpret_cast<sung::TVec3<T>*>(&v);
+    }
+
+    template <typename T>
+    const sung::TVec3<T>& vec_cast(const glm::tvec3<T>& v) {
+        return *reinterpret_cast<const sung::TVec3<T>*>(&v);
+    }
+
 }  // namespace dal
