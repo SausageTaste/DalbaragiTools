a5f8d6ca4d858cfb9d7a3602ceb704bd49de997c
Sungmin Woo <woos8899@gmail.com>
2025-01-17 22:56:54 +0900

    Edit portfile to copy license file

--- vcpkg/ports/sungtools/portfile.cmake
+++ vcpkg/ports/sungtools/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     SHA512 ae74a7c9b40e3a7222932bb0c3f3f0e5dd59168838d4f157e74e278938824ae0857bc5262aa4954237d2ac5328e30f29a3c0b6a4f33a284c929f420fe475f528
 )
 
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/LICENSE)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
