a6c044e5b6528988d6a8446764f0590e3d0ae1c2
Sungmin Woo <woos8899@gmail.com>
2025-01-17 22:35:18 +0900

    Update `sungtools` port

--- vcpkg/ports/sungtools/portfile.cmake
+++ vcpkg/ports/sungtools/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO SausageTaste/SungToolsCpp
+    REPO SausageTaste/sungtools
     REF v0.1.1
     SHA512 ae74a7c9b40e3a7222932bb0c3f3f0e5dd59168838d4f157e74e278938824ae0857bc5262aa4954237d2ac5328e30f29a3c0b6a4f33a284c929f420fe475f528
 )
